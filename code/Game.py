0a6478742ed06ee753027cbb90a79314ff76aad5
Marcelo <fullcodebase@gmail.com>
2026-02-28 15:27:54 -0300

    Adding assets Assing Music and Image

--- code/Game.py
+++ code/Game.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH
+from code.Const import WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
 
     def run(self, ):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
         while True:
             menu = Menu(self.window)
             menu.run()
